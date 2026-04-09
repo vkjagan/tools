4cc08f6472b19558619078863bbdb5873a9d7547
Jaganmohan <vkjagan@gmail.com>
2026-04-09 16:53:49 +0530

    Add visible generator version badges to output pages

---
+++ album/ads.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Album static generator ad configuration.
+ * Fill your AdSense client + slot IDs and set enabled=true.
+ */
+return [
+    'enabled' => false,
+    'adsense_client' => 'ca-pub-XXXXXXXXXXXXXXXX',
+    'slots' => [
+        // Grid pages
+        'grid_top_desktop' => '',
+        'grid_top_mobile' => '',
+        'grid_bottom_desktop' => '',
+        'grid_bottom_mobile' => '',
+
+        // Album pages
+        'album_top_desktop' => '',
+        'album_top_mobile' => '',
+        'album_mid_desktop' => '',
+        'album_mid_mobile' => '',
+        'album_footer_desktop' => '',
+        'album_footer_mobile' => '',
+    ],
+];
